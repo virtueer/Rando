961ea9fd6a528ed7900e4232f366c1beecc7d454
unknown <erdemefesteam2@hotmail.com>
2022-12-18 21:21:29 +0300

    should length exclude matched sockets test waiting

--- __test__/socket/index.test.js
+++ __test__/socket/index.test.js
@@ -41,73 +41,116 @@ describe('tdd socket tests', () => {
     clients.forEach(c => c.close())
   });
 
-  afterEach(() => {
-    clients.forEach(c => c.off())
+  describe("length and message arriving tests", () => {
+    afterEach(() => {
+      clients.forEach(c => c.off())
+    })
+
+    afterAll(() => {
+      clients.forEach(c => c.close())
+      clients.splice(0)
+    });
+
+
+    test("should return length 1", asyncWithDone(async done => {
+      const client = await addClient(clients)
+
+      client.on('getLength', length => {
+        expect(length).toBe(1)
+        expect(clients.length).toBe(1)
+        done()
+      })
+
+      client.emit('getLength')
+    }));
+
+    test("should return length 2", asyncWithDone(async done => {
+      const client = await addClient(clients)
+      client.on('getLength', length => {
+        expect(length).toBe(2)
+        expect(clients.length).toBe(2)
+        done()
+      })
+
+      client.emit('getLength')
+    }));
+
+    test("should update length realtime for one client", asyncWithDone(async done => {
+      const client = addClient(clients)
+      clients[0].on('length', async length => {
+        expect(length).toBe(3)
+        await client // without this following expect fails
+        expect(clients.length).toBe(3)
+        done()
+      })
+    }))
+
+    test("should update length realtime for all clients", asyncWithDone(async done => {
+      const times = jest.fn()
+      clients.forEach(c => c.on('length', times))
+      await addClient(clients)
+      expect(times).toHaveBeenCalledTimes(3)
+      done()
+    }))
+
+    test("should match someone", asyncWithDone(async done => {
+      const message = 'hello world'
+      const index = Math.floor(Math.random() * (clients.length - 1));
+      const mockFunc = jest.fn()
+      let times = 0
+      clients.forEach(c => {
+        c.on('matched', (room) => {
+          c.emit('message', { room, message })
+        })
+
+        c.on('message', message => {
+          mockFunc(message)
+          if (++times === 2) {
+            expect(mockFunc.mock.calls.length).toBe(2)
+            expect(mockFunc.mock.calls).toEqual([[message], [message]])
+            done()
+          }
+        })
+      })
+      const client = clients[index]
+      client.emit('match')
+    }))
   })
 
 
-  test("should return length 1", asyncWithDone(async done => {
-    const client = await addClient(clients)
+  describe("matching tests", () => {
+    test("should return length 4", asyncWithDone(async done => {
+      const client = await addClient(clients)
+      await addClient(clients)
+      await addClient(clients)
+      await addClient(clients)
 
-    client.on('getLength', length => {
-      expect(length).toBe(1)
-      expect(clients.length).toBe(1)
-      done()
-    })
-
-    client.emit('getLength')
-  }));
-
-  test("should return length 2", asyncWithDone(async done => {
-    const client = await addClient(clients)
-    client.on('getLength', length => {
-      expect(length).toBe(2)
-      expect(clients.length).toBe(2)
-      done()
-    })
-
-    client.emit('getLength')
-  }));
-
-  test("should update length realtime for one client", asyncWithDone(async done => {
-    const client = addClient(clients)
-    clients[0].on('length', async length => {
-      expect(length).toBe(3)
-      await client // without this following expect fails
-      expect(clients.length).toBe(3)
-      done()
-    })
-  }))
-
-  test("should update length realtime for all clients", asyncWithDone(async done => {
-    const times = jest.fn()
-    clients.forEach(c => c.on('length', times))
-    await addClient(clients)
-    expect(times).toHaveBeenCalledTimes(3)
-    done()
-  }))
-
-  test("should match someone", asyncWithDone(async done => {
-    const message = 'hello world'
-    const index = Math.floor(Math.random() * (clients.length - 1));
-    const mockFunc = jest.fn()
-    let times = 0
-    clients.forEach(c => {
-      c.on('matched', (room) => {
-        c.emit('message', { room, message })
+      client.on('getLength', length => {
+        expect(length).toBe(4)
+        expect(clients.length).toBe(4)
+        done()
       })
 
-      c.on('message', message => {
-        mockFunc(message)
-        if (++times === 2) {
-          expect(mockFunc.mock.calls.length).toBe(2)
-          console.log(mockFunc.mock.calls)
-          expect(mockFunc.mock.calls).toEqual([[message], [message]])
-          done()
-        }
+      client.emit('getLength')
+    }));
+
+    test('should length exclude matched sockets', asyncWithDone(async done => {
+      const [client1, client2, client3, client4] = clients
+
+      [client1, clien2].forEach(c => {
+        c.on('matched', () => {
+          client3.emit('getLength', l => {
+            expect(l).toBe(2)
+          })
+
+          client4.emit('getLength', l => {
+            expect(l).toBe(2)
+          })
+        })
       })
-    })
-    const client = clients[index]
-    client.emit('match')
-  }))
+
+      client1.emit('match')
+      client2.emit('match')
+    }))
+  })
 })
